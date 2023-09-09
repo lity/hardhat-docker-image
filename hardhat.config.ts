bf3afc869b7be8a69cb46a7993d50d03e9bc999f
lity <exceeding@gmail.com>
2023-09-09 11:41:12 +0800

    update hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,9 @@
-import { ethers } from 'ethers'
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
 
-task('set-balance', 'set balance of an address to some value')
-  .addPositionalParam('address')
-  .addPositionalParam('value')
-  .setAction(async (taskArgs, hre) => {
-    console.log(`network: ${hre.network.name}`)
-
-    if (hre.network.name == 'localhost') {
-      await hre.network.provider.send('hardhat_setBalance', [
-        taskArgs.address,
-        ethers.utils.parseEther(`${taskArgs.value}`).toHexString().replace(/^0x0+/, '0x'),
-      ])
-      console.log(`balance of ${taskArgs.address}: `, await hre.ethers.provider.getBalance(taskArgs.address))
-    } else {
-      console.log(`hardhat_setBalance is unavailable on non-hardhat network`)
-    }
-  })
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.19',
   defaultNetwork: 'localhost',
   networks: {
     localhost: {
@@ -28,6 +11,6 @@ const config: HardhatUserConfig = {
       chainId: 31337,
     }
   }
-};
+}
 
-export default config;
+export default config
